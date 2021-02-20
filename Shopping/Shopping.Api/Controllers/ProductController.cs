0d14932c809b654f63651a0bb9b96d0e44bf1178
panospd <panos.anastasiadis@cloudcommercegroup.com>
2021-02-20 12:07:53 +0000

    Initial commit

---
+++ Shopping/Shopping.Api/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Shopping.Api.Data;
+using Shopping.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shopping.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _context;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext productContext)
+        {
+            _logger = logger;
+            _context = productContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products
+                .Find(p => true)
+                .ToListAsync();
+        }
+    }
+}
